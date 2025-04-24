07b3066e294fd8dc942e646d017114e8d6878534
RianDantas <134387076+RianDantas@users.noreply.github.com>
2025-04-24 18:22:37 +0000

    dart

---
+++ TiposEOperadores.dart
@@ -0,0 +1,17 @@
+void main(){
+
+  int idade = 28;
+  double altura = 1.75;
+  String nomeAnimal = "Pitoco";
+
+  bool ligada = false;
+
+  var a = 10;
+  var b = 3;
+  var soma = a + b;
+  var divisao = a%b;
+
+  double desconto = 200 - (200 * 0.10);
+  print(desconto);
+
+}
